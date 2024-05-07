66c2df21349505f26ef544921bca02b2cdd5537e
celularfeak <celularfreak@hotmail.com>
2024-05-07 19:30:39 +0200

    security

--- src/main/java/com/MovieReviews/moviereviews/MoviereviewsApplication.java
+++ src/main/java/com/MovieReviews/moviereviews/MoviereviewsApplication.java
@@ -1,7 +1,16 @@
 package com.MovieReviews.moviereviews;
 
+import com.MovieReviews.moviereviews.security.models.Role;
+import com.MovieReviews.moviereviews.security.models.User;
+import com.MovieReviews.moviereviews.security.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class MoviereviewsApplication {
@@ -10,4 +19,27 @@ public class MoviereviewsApplication {
 		SpringApplication.run(MoviereviewsApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+
+			userService.saveUser(new User(null, "Emma Watson", "emma", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Robert Downey Jr.", "robert", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Angelina Jolie", "angelina", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Leonardo DiCaprio", "leo", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("emma", "ROLE_USER");
+			userService.addRoleToUser("robert", "ROLE_USER");
+			userService.addRoleToUser("robert", "ROLE_ADMIN");
+			userService.addRoleToUser("angelina", "ROLE_USER");
+			userService.addRoleToUser("leo", "ROLE_USER");
+		};
+	}
 }
